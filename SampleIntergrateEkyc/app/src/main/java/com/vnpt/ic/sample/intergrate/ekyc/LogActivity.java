5eb9d1be38f21fdbe258daba728443b7c840019b
phi quoc hung <phiquochung@vnpt.vn>
2025-05-16 16:50:29 +0700

    Update version only ekyc

--- SampleIntergrateEkyc/app/src/main/java/com/vnpt/ic/sample/intergrate/ekyc/LogActivity.java
+++ SampleIntergrateEkyc/app/src/main/java/com/vnpt/ic/sample/intergrate/ekyc/LogActivity.java
@@ -6,6 +6,7 @@ import static com.vnptit.idg.sdk.utils.KeyResultConstants.LIVENESS_CARD_FRONT_RE
 import static com.vnptit.idg.sdk.utils.KeyResultConstants.LIVENESS_FACE_RESULT;
 import static com.vnptit.idg.sdk.utils.KeyResultConstants.MASKED_FACE_RESULT;
 import static com.vnptit.idg.sdk.utils.KeyResultConstants.OCR_RESULT;
+import static com.vnptit.idg.sdk.utils.KeyResultConstants.QR_CODE_RESULT;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -19,7 +20,14 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
 import com.vnpt.ic.sample.intergrate.ekyc.databinding.ActivityLogResultBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.StringReader;
+
 
 public class LogActivity extends AppCompatActivity {
 
@@ -38,6 +46,8 @@ public class LogActivity extends AppCompatActivity {
       setLogResultSafe(binding.logCompare, intent.getStringExtra(COMPARE_FACE_RESULT));
       setLogResultSafe(binding.logLivenessFace, intent.getStringExtra(LIVENESS_FACE_RESULT));
       setLogResultSafe(binding.logMaskFace, intent.getStringExtra(MASKED_FACE_RESULT));
+      setLogResultSafe(binding.logQr, intent.getStringExtra(QR_CODE_RESULT));
+
 
       binding.close.setOnClickListener(v -> finish());
 
@@ -48,6 +58,7 @@ public class LogActivity extends AppCompatActivity {
          copy(intent.getStringExtra(COMPARE_FACE_RESULT));
          copy(intent.getStringExtra(LIVENESS_FACE_RESULT));
          copy(intent.getStringExtra(MASKED_FACE_RESULT));
+         copy(intent.getStringExtra(QR_CODE_RESULT));
       });
    }
 
@@ -60,12 +71,36 @@ public class LogActivity extends AppCompatActivity {
    private void setLogResultSafe(final LogResultView view, final String logResult) {
       view.setVisibility(View.VISIBLE);
       if (!TextUtils.isEmpty(logResult)) {
-         Gson gson = new GsonBuilder().setPrettyPrinting().create();
-         JsonElement je = new JsonParser().parse(logResult);
-         String prettyJsonString = gson.toJson(je);
-         view.setLogResult(prettyJsonString);
+         if (isJson(logResult)) {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            JsonReader reader = new JsonReader(new StringReader(logResult));
+            reader.setLenient(true); // Allow lenient parsing
+            JsonElement je = JsonParser.parseReader(reader);
+            String prettyJsonString = gson.toJson(je);
+            view.setLogResult(prettyJsonString);
+         } else {
+            view.setLogResult(logResult);
+         }
       } else {
          view.setVisibility(View.GONE);
+      }
+   }
+
+   private boolean isJson(String str) {
+      if (str == null || str.isEmpty()) {
+         return false;
+      }
+      try {
+         new JSONObject(str);
+         return true;
+      } catch (JSONException e) {
+         try {
+            // Try parsing as a JSON array as well
+            new org.json.JSONArray(str);
+            return true;
+         } catch (JSONException e2) {
+            return false;
+         }
       }
    }
 }
